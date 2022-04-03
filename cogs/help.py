25edcc854a7ca9df7bb5ac7ab4e599e05d43878b
Tyzie <101468187+Tyzie@users.noreply.github.com>
2022-04-03 09:24:35 +0300

    Add files via upload

--- cogs/help.py
+++ cogs/help.py
@@ -8,6 +8,7 @@ cog.labeler.vbml_ignore_case = True
 @cog.on.message(text=["помощь"])
 async def help(message: Message):
 	user = await cog.api.users.get(message.from_id)
+	player = Player.get_profile(user[0].id)
 	await message.answer(f"""Мои команды, [id{player.id}|{player.nickname}] [{player.uid}]
 		&#12288;Профиль
 		&#12288;Помощь""", keyboard=mainkeyb)
