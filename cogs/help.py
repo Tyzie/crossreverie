9d95813e6fb408d8e4c2176658d62286e14c1d89
Tyzie <101468187+Tyzie@users.noreply.github.com>
2022-04-03 17:12:18 +0300

    Add files via upload

--- cogs/help.py
+++ cogs/help.py
@@ -11,22 +11,24 @@ async def help(message: Message):
 	player = Player.get_profile(user[0].id)
 	if player != False and player.keyb == 1:
 		await message.answer(f"""Мои команды, [id{player.id}|{player.nickname}] [{player.uid}]
-			&#12288;Профиль
-			&#12288;Помощь
-			&#12288;⚙ Настройки
-			&#12288;Ник [новый ник]
-			&#12288;Заработок
-			&#12288;Развлечения""", keyboard=mainkeyb)
+&#12288;Профиль
+&#12288;Помощь
+&#12288;⚙ Настройки
+&#12288;Ник [новый ник]
+&#12288;Заработок
+&#12288;Развлечения
+""", keyboard=mainkeyb)
 		Player.set_action(player.uid, "main")
 		print(f"{player.nickname} [{player.uid}] called 'help'")
 	if player != False and player.keyb == 0:
 		await message.answer(f"""Мои команды, [id{player.id}|{player.nickname}] [{player.uid}]
-			&#12288;Профиль
-			&#12288;Помощь
-			&#12288;⚙ Настройки
-			&#12288;Ник [новый ник]
-			&#12288;Заработок
-			&#12288;Развлечения""", keyboard=EMPTY_KEYBOARD)
+&#12288;Профиль
+&#12288;Помощь
+&#12288;⚙ Настройки
+&#12288;Ник [новый ник]
+&#12288;Заработок
+&#12288;Развлечения
+""", keyboard=EMPTY_KEYBOARD)
 		Player.set_action(player.uid, "main")
 		print(f"{player.nickname} [{player.uid}] called 'help'")
 	
