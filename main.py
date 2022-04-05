4f11b82d51ec139386b1460b79d29dc40d8aea77
Tyzie <101468187+Tyzie@users.noreply.github.com>
2022-04-05 19:10:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from vkbottle import Bot, load_blueprints_from_package
+from vkbottle import API, load_blueprints_from_package, ABCResponseValidator, ABCRequestValidator
+from typing import Any, NoReturn, Union
+from vkbottle.bot import Bot
 import psycopg2 as ps
 from config import token, DB_URI
 import os
 import time
 import asyncio
-from .cogs import bps
 
 bot = Bot(token)
+api = API(token)
 
-for cog in bps:
+for cog in load_blueprints_from_package("cogs"):
     cog.load(bot)
 
+class SomeResponseValidator(ABCResponseValidator):
+    async def validate(self, response: dict) -> Union[Any, NoReturn]:
+        # some stuff with response
+        return response
+
+class SomeRequestValidator(ABCRequestValidator):
+    async def validate(self, request: dict) -> dict:
+        # some stuff with request data
+        return request
+
 bot.run_forever()
+api.response_validators.append(SomeResponseValidator())
+api.request_validators.append(SomeRequestValidator())
