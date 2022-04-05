96aa7b743e3cfc6257a54168d4f4d26060b60b65
Tyzie <101468187+Tyzie@users.noreply.github.com>
2022-04-05 17:41:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from vkbottle import Bot, load_blueprints_from_package
 import psycopg2 as ps
-from config import token
+from config import token, DB_URI
+import os
 import time
+import asyncio
 
 bot = Bot(token)
 
 for cog in load_blueprints_from_package("cogs"):
+	asyncio.sleep(1)
     cog.load(bot)
-
-time.sleep(3)
 
 bot.run_forever()
